2a0c318b9031001cfba08d583bbb8df4d0f892ab
enbraining <me@fodo.dev>
2024-11-06 15:55:13 +0900

    모듈 최소화

--- typeorm-transaction/src/app.module.ts
+++ typeorm-transaction/src/app.module.ts
@@ -4,12 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigsModule } from './configs/configs.module';
 import { typeOrmConfig } from './configs/typeorm.config';
-import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
 import { ItemsModule } from './items/items.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigsModule, TypeOrmModule.forRoot(typeOrmConfig), ProductsModule, OrdersModule, ItemsModule],
+  imports: [
+    ConfigsModule,
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ProductsModule,
+    OrdersModule,
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- typeorm-transaction/src/products/dto/create-product.dto.ts
+++ typeorm-transaction/src/products/dto/create-product.dto.ts
@@ -1,9 +1,4 @@
-import { IsNotEmpty } from 'class-validator';
-
 export class CreateProductDto {
-  @IsNotEmpty()
   title: string;
-
-  @IsNotEmpty()
   price: number;
 }
